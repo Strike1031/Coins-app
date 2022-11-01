428b6a965b32944a31158dc5e111ac8a8be89aee
Strike <strike10310522@gmail.com>
2022-11-01 09:39:13 -0700

    getPriceGroupbyBlock

--- src/components/coins/TableCoins.tsx
+++ src/components/coins/TableCoins.tsx
@@ -45,53 +45,53 @@ const TableCoins = () => {
   return (
     <>
       <div className='overflow-x-auto'>
-        <table className='overflow-y-hidden table-auto text-xs'>
+        <table className='overflow-y-hidden text-xs table-auto'>
           <thead>
             <tr>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-4 py-6 text-black-500 text-center lg:w-1/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 px-4 py-6 font-bold text-center border-t border-b lg:w-1/12 dark:border-neutral-80'>
                 #
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold pl-2 pr-6 py-6 text-black-500 lg:w-4/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 py-6 pr-6 pl-2 font-bold border-t border-b lg:w-4/12 dark:border-neutral-80'>
                 Name
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-4 py-6 text-black-500 text-right dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 px-4 py-6 font-bold text-right border-t border-b dark:border-neutral-80'>
                 Price
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-4 py-6 text-black-500 whitespace-nowrap lg:w-2/12 dark:border-neutral-80'>
-                Price Changes
+              <td className='border-neutral-30 text-black-500 flex-1 px-4 py-6 font-bold whitespace-nowrap border-t border-b lg:w-2/12 dark:border-neutral-80'>
+                5min %
               </td>
 
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-2 py-6 text-black-500 text-center dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 px-2 py-6 font-bold text-center border-t border-b dark:border-neutral-80'>
                 1H %
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-2 py-6 text-black-500 text-center whitespace-nowrap dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 px-2 py-6 font-bold text-center whitespace-nowrap border-t border-b dark:border-neutral-80'>
+                6H %
+              </td>
+              <td className='border-neutral-30 text-black-500 flex-1 px-2 py-6 font-bold text-center border-t border-b dark:border-neutral-80'>
                 24H %
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-2 py-6 text-black-500 text-center dark:border-neutral-80'>
-                7d %
-              </td>
-              <td className='border-b border-neutral-30 border-t flex-1 flex-grow font-bold items-center min-w-min px-4 py-6 text-black-500 lg:w-2/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 flex-grow items-center px-4 py-6 min-w-min font-bold border-t border-b lg:w-2/12 dark:border-neutral-80'>
                 <div className='flex items-center whitespace-nowrap'>
                   Market Cap
-                  <BsFillExclamationCircleFill className='h-4 ml-1 text-neutral-40 w-4' />
+                  <BsFillExclamationCircleFill className='text-neutral-40 ml-1 w-4 h-4' />
                 </div>
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 flex-grow font-bold items-center min-w-min px-4 py-6 text-black-500 lg:w-2/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 flex-grow items-center px-4 py-6 min-w-min font-bold border-t border-b lg:w-2/12 dark:border-neutral-80'>
                 <div className='flex items-center whitespace-nowrap'>
                   Volume
-                  <BsFillExclamationCircleFill className='h-4 ml-1 text-neutral-40 w-4' />
+                  <BsFillExclamationCircleFill className='text-neutral-40 ml-1 w-4 h-4' />
                 </div>
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 flex-grow font-bold items-center min-w-min px-4 py-6 text-black-500 lg:w-2/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 flex-grow items-center px-4 py-6 min-w-min font-bold border-t border-b lg:w-2/12 dark:border-neutral-80'>
                 <div className='flex items-center whitespace-nowrap'>
-                  Supply
-                  <BsFillExclamationCircleFill className='h-4 ml-1 text-neutral-40 w-4' />
+                  Txns
+                  <BsFillExclamationCircleFill className='text-neutral-40 ml-1 w-4 h-4' />
                 </div>
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 flex-grow font-bold min-w-min px-4 py-6 text-black-500 lg:w-2/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 flex-grow px-4 py-6 min-w-min font-bold border-t border-b lg:w-2/12 dark:border-neutral-80'>
                 Liquidity
               </td>
-              <td className='border-b border-neutral-30 border-t flex-1 font-bold px-3 py-6 text-black-500 text-right lg:w-2/12 dark:border-neutral-80'>
+              <td className='border-neutral-30 text-black-500 flex-1 px-3 py-6 font-bold text-right border-t border-b lg:w-2/12 dark:border-neutral-80'>
                 Graph
               </td>
             </tr>
@@ -103,22 +103,22 @@ const TableCoins = () => {
             ).map((coin, index) => (
               <tr
                 key={index}
-                className='border-b border-neutral-30 dark:border-neutral-80'
+                className='border-neutral-30 border-b dark:border-neutral-80'
               >
-                <td className='align-middle px-4 py-7 whitespace-nowrap lg:px-0'>
+                <td className='px-4 py-7 align-middle whitespace-nowrap lg:px-0'>
                   <div className='flex items-center space-x-2 w-full'>
-                    <AiOutlineStar className='h-4 mr-2 text-neutral-60 w-4' />
+                    <AiOutlineStar className='text-neutral-60 mr-2 w-4 h-4' />
                     {/* write coins number base on paginationPage  */}
                     {index + 1 + paginationPage * 10}
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
                   <button
                     type='button'
                     className='flex items-center w-full'
                     data-hs-overlay='#coinDetails-overlay'
                   >
-                    <div className='h-8 mr-2 w-8'>
+                    <div className='mr-2 w-8 h-8'>
                       <NextImage
                         alt='Bitcoin'
                         src={coin.logo}
@@ -126,28 +126,28 @@ const TableCoins = () => {
                         height={32}
                       />
                     </div>
-                    <p className='font-medium mr-1 text-sm'>{coin.name}</p>
-                    <p className='capitalize font-medium text-neutral-50 text-sm'>
+                    <p className='mr-1 text-sm font-medium'>{coin.name}</p>
+                    <p className='text-neutral-50 text-sm font-medium capitalize'>
                       {coin.code}
                     </p>
                   </button>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
                   <div className='flex items-center w-full'>
-                    <p className='font-medium text-sm'>{coin.price}</p>
+                    <p className='text-sm font-medium'>{coin.price}</p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
-                  <div className='flex items-center justify-center w-full'>
-                    <p className='font-medium text-sm'>{coin.priceChange}</p>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
+                  <div className='flex justify-center items-center w-full'>
+                    <p className='text-sm font-medium'>{coin.priceChange}</p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
                   <div className='flex items-center space-x-1 w-full'>
                     {coin['1h']?.change === 'down' ? (
-                      <FaChevronDown className='h-3 text-red-500 w-3 dark:text-red-semantic' />
+                      <FaChevronDown className='w-3 h-3 text-red-500 dark:text-red-semantic' />
                     ) : (
-                      <FaChevronUp className='h-3 text-green-500 w-3' />
+                      <FaChevronUp className='w-3 h-3 text-green-500' />
                     )}
                     <p
                       className={clsxm(
@@ -161,12 +161,12 @@ const TableCoins = () => {
                     </p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
                   <div className='flex items-center space-x-1 w-full'>
                     {coin['24h']?.change === 'down' ? (
-                      <FaChevronDown className='h-3 text-red-500 w-3 dark:text-red-semantic' />
+                      <FaChevronDown className='w-3 h-3 text-red-500 dark:text-red-semantic' />
                     ) : (
-                      <FaChevronUp className='h-3 text-green-500 w-3' />
+                      <FaChevronUp className='w-3 h-3 text-green-500' />
                     )}
                     <p
                       className={clsxm(
@@ -180,12 +180,12 @@ const TableCoins = () => {
                     </p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
                   <div className='flex items-center space-x-1 w-full'>
                     {coin['7d']?.change === 'down' ? (
-                      <FaChevronDown className='h-3 text-red-500 w-3 dark:text-red-semantic' />
+                      <FaChevronDown className='w-3 h-3 text-red-500 dark:text-red-semantic' />
                     ) : (
-                      <FaChevronUp className='h-3 text-green-500 w-3' />
+                      <FaChevronUp className='w-3 h-3 text-green-500' />
                     )}
                     <p
                       className={clsxm(
@@ -199,32 +199,32 @@ const TableCoins = () => {
                     </p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 text-right whitespace-nowrap lg:px-4'>
-                  <div className='flex items-center justify-end space-x-1 w-full'>
-                    <p className='font-medium text-sm'>{coin.marketCap}</p>
+                <td className='px-4 py-2 text-right align-middle whitespace-nowrap lg:px-4'>
+                  <div className='flex justify-end items-center space-x-1 w-full'>
+                    <p className='text-sm font-medium'>{coin.marketCap}</p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
-                  <div className='flex items-center justify-end space-x-1 w-full'>
-                    <p className='font-medium text-sm'>{coin.volume}</p>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
+                  <div className='flex justify-end items-center space-x-1 w-full'>
+                    <p className='text-sm font-medium'>{coin.volume}</p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
-                  <div className='flex items-center justify-end space-x-1 w-full'>
-                    <p className='font-medium text-sm'>{coin.supply}</p>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
+                  <div className='flex justify-end items-center space-x-1 w-full'>
+                    <p className='text-sm font-medium'>{coin.supply}</p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:px-4'>
-                  <div className='flex items-center justify-end space-x-1 w-full'>
-                    <p className='font-medium text-sm'>{coin.liquidity}</p>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:px-4'>
+                  <div className='flex justify-end items-center space-x-1 w-full'>
+                    <p className='text-sm font-medium'>{coin.liquidity}</p>
                   </div>
                 </td>
-                <td className='align-middle px-4 py-2 whitespace-nowrap lg:pl-4'>
-                  <div className='flex items-center justify-end w-full'>
+                <td className='px-4 py-2 align-middle whitespace-nowrap lg:pl-4'>
+                  <div className='flex justify-end items-center w-full'>
                     {coin.graph === 'down' ? (
-                      <GraphDown className='h-8 w-auto' />
+                      <GraphDown className='w-auto h-8' />
                     ) : (
-                      <GraphUp className='h-8 w-auto' />
+                      <GraphUp className='w-auto h-8' />
                     )}
                   </div>
                 </td>
@@ -233,14 +233,14 @@ const TableCoins = () => {
           </tbody>
         </table>
       </div>
-      <div className='flex flex-col mt-2 sm:flex-row lg:items-center lg:justify-between lg:mb-[88px] lg:mt-8'>
-        <p className='font-medium mt-4 order-2 text-sm lg:mt-0 lg:order-1'>
+      <div className='flex flex-col mt-2 sm:flex-row lg:mb-[88px] lg:justify-between lg:items-center lg:mt-8'>
+        <p className='order-2 mt-4 text-sm font-medium lg:order-1 lg:mt-0'>
           {' '}
           Showing {paginationPage + 1} to 10 of {Coins?.length} entries
         </p>
-        <div className='flex flex-nowrap font-medium justify-end min-w-min mt-4 order-1 overflow-x-auto self-start space-x-3 text-sm w-full sm:mt-0 lg:order-2 lg:overflow-hidden lg:self-auto'>
+        <div className='flex overflow-x-auto flex-nowrap order-1 justify-end self-start mt-4 space-x-3 w-full min-w-min text-sm font-medium sm:mt-0 lg:overflow-hidden lg:order-2 lg:self-auto'>
           <button
-            className='border border-neutral-30 px-3 py-2 rounded-lg text-xs dark:border-neutral-80'
+            className='border-neutral-30 px-3 py-2 text-xs rounded-lg border dark:border-neutral-80'
             onClick={() => {
               if (paginationPage) {
                 setPaginationPage(paginationPage - 1);
@@ -253,7 +253,7 @@ const TableCoins = () => {
           {renderPageNumbers}
 
           <button
-            className='border border-neutral-30 px-3 py-2 rounded-lg text-xs dark:border-neutral-80'
+            className='border-neutral-30 px-3 py-2 text-xs rounded-lg border dark:border-neutral-80'
             onClick={() => {
               if (paginationPage < Coins.length / 10 - 1) {
                 setPaginationPage(paginationPage + 1);
